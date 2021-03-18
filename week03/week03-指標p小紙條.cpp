c38bfa3aa8e1d315598a566de48a7e154faeb3aa
suv5225 <79676902+suv5225@users.noreply.github.com>
2021-03-18 16:23:55 +0800

    Rename week03-指標p小紙條.cpp to week03/week03-指標p小紙條.cpp

--- week03/week03-指標p小紙條.cpp
+++ week03/week03-指標p小紙條.cpp
@@ -19,16 +19,16 @@ int main()
     int *p = &a[2];
     *p = 222;
     printAll();
-    printf("p¤ß¸Ìªº¤p¯È±ø°Oªº­È¬O:%d\n",p);
+    printf("på¿ƒè£¡çš„å°ç´™æ¢è¨˜çš„å€¼æ˜¯:%d\n",p);
 
     p = p + 2;
     *p = 666;
     printAll();
-    printf("p¤ß¸Ìªº¤p¯È±ø°Oªº­È¬O:%d\n",p);
+    printf("på¿ƒè£¡çš„å°ç´™æ¢è¨˜çš„å€¼æ˜¯:%d\n",p);
 
     p--;
     *p = 555;
     printAll();
-    printf("p¤ß¸Ìªº¤p¯È±ø°Oªº­È¬O:%d\n",p);
+    printf("på¿ƒè£¡çš„å°ç´™æ¢è¨˜çš„å€¼æ˜¯:%d\n",p);
 
 }
