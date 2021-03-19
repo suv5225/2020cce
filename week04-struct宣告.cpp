b9d607d25e9d42af7b558f84be1bcc1688e9598c
suv5225 <79676902+suv5225@users.noreply.github.com>
2021-03-19 11:47:52 +0800

    Add files via upload

---
+++ week04-struct宣告.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+struct POINT
+{
+    float x , y;
+};
+
+int main()
+{
+    struct POINT a;
+
+}
---
+++ week04-struct陣列a與c=b.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+struct DATA
+{
+    int x , y;
+} a[3];
+
+struct DATA b = { 100 , 200 };
+
+int main()
+{
+
+    for( int i = 0 ; i < 3 ; i++ )
+    {
+        printf("a[%d]:%d %d\n", i , a[i].x , a[i].y );
+    }
+
+    printf("b: %d %d\n", b.x , b.y );
+
+    struct DATA c;
+    printf("c: %d %d 像亂碼\n", c.x , c.y );
+
+    c = b;
+    /*
+        舊式寫法   c.x = b.x;
+                   c.y = b.y;
+    */
+
+    printf("c: %d %d\n", c.x , c.y );
+}
